ff512895df0fbeed51ee723c5d33c4c363667645
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2022-12-29 15:31:04 -0300

    Segundo commit

---
+++ src/Minterfaces/test/MetodoGenericoTest01.java
@@ -0,0 +1,16 @@
+package Minterfaces.test;
+
+import Minterfaces.DominioGenerics.Barco;
+
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrayComUmObjeto(new Barco("Canoa Marota"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t){
+        return List.of(t);
+    }
+}
