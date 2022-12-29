46cb116061acab1841395fa481db3f4252d7f44b
Luis Felipe Rodrigues <71678443+Luisfelipeqt@users.noreply.github.com>
2022-12-29 15:25:00 -0300

    Delete src/Minterfaces/dominio directory

--- src/Minterfaces/dominio/DataLoader.java
+++
@@ -1,8 +0,0 @@
-package Minterfaces.dominio;
-
-public interface DataLoader {
-    public abstract void load();
-     default void checkBancodeDados(){
-        System.out.println("Checando Banco de Dados");
-    }
-}
